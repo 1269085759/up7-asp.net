295aa4eae399f5f6076fa6ddbf92ac506bdc084e
qwl <qwl@ncmem.com>
2016-10-09 17:51:57 +0800

    清除目录时取消\\，如果报错：“目录不是空的”，需要给upload文件夹增加Administrator完全控制权限

--- demoSql2005/db/clear.aspx.cs
+++ demoSql2005/db/clear.aspx.cs
@@ -13,7 +13,7 @@ namespace up6.demoSql2005.db
 
             //删除upload文件夹
             PathBuilder pb = new PathBuilder();
-            string pathSvr = pb.getRoot()+"\\";
+            string pathSvr = pb.getRoot();
             if(Directory.Exists(pathSvr)) Directory.Delete(pathSvr,true);
         }
     }
