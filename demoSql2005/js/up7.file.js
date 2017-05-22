5453f99b9028bed9f8145c98f07feb259d82580b
qwl <qwl@ncmem.com>
2017-05-22 17:05:44 +0800

    增加AutoMerge参数

--- demoSql2005/js/up7.file.js
+++ demoSql2005/js/up7.file.js
@@ -146,7 +146,7 @@ function FileUploader(fileLoc, mgr)
         //从未上传列表中删除
         this.Manager.RemoveQueueWait(this.fileSvr.idSign);
 
-        var param = { idSign: this.fileSvr.idSign, uid: this.uid, time: new Date().getTime() };
+        var param = { idSign: this.fileSvr.idSign, uid: this.uid, merge: this.Config.AutoMerge, time: new Date().getTime() };
 
         $.ajax({
             type: "GET"
