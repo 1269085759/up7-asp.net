649c9bfff8256a4da6d28e58a85474216161d289
qwl <qwl@ncmem.com>
2017-08-04 17:16:18 +0800

    文件表添加方法更新

--- demoSql2005/db/biz/database/DBFile.cs
+++ demoSql2005/db/biz/database/DBFile.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Data.Common;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace up7.demoSql2005.db
 {
@@ -76,6 +77,84 @@ namespace up7.demoSql2005.db
             db.AddString(ref cmd, "@f_sizeLoc", model.sizeLoc, 15);
 
             db.ExecuteNonQuery(cmd);
+        }
+        public void add(ref xdb_files f)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("insert into up7_files(");
+            sb.Append(" f_idSign");
+            sb.Append(",f_uid");
+            sb.Append(",f_nameLoc");
+            sb.Append(",f_nameSvr");
+            sb.Append(",f_pathLoc");
+            sb.Append(",f_pathSvr");
+            sb.Append(",f_pathRel");
+            sb.Append(",f_blockCount");
+            sb.Append(",f_blockSize");
+            sb.Append(",f_blockPath");
+            sb.Append(",f_lenLoc");
+            sb.Append(",f_sizeLoc");
+
+            sb.Append(") values (");
+
+            sb.Append(" @f_idSign");
+            sb.Append(",@f_uid");
+            sb.Append(",@f_nameLoc");
+            sb.Append(",@f_nameSvr");
+            sb.Append(",@f_pathLoc");
+            sb.Append(",@f_pathSvr");
+            sb.Append(",@f_pathRel");
+            sb.Append(",@f_blockCount");
+            sb.Append(",@f_blockSize");
+            sb.Append(",@f_blockPath");
+            sb.Append(",@f_lenLoc");
+            sb.Append(",@f_sizeLoc");
+            sb.Append(") ;");
+
+            DbHelper db = new DbHelper();
+            DbCommand cmd = db.GetCommand(sb.ToString());
+
+            db.AddString(ref cmd, "@f_idSign", f.idSign, 36);
+            db.AddInt(ref cmd, "@f_uid", f.uid);
+            db.AddString(ref cmd, "@f_nameLoc", f.nameLoc, 255);
+            db.AddString(ref cmd, "@f_nameSvr", f.nameSvr, 255);
+            db.AddString(ref cmd, "@f_pathLoc", f.pathLoc, 512);
+            db.AddString(ref cmd, "@f_pathSvr", f.pathSvr, 512);
+            db.AddString(ref cmd, "@f_pathRel", f.pathRel, 512);
+            db.AddInt(ref cmd, "@f_blockCount", f.blockCount);
+            db.AddInt(ref cmd, "@f_blockSize", f.blockSize);
+            db.AddString(ref cmd, "@f_blockPath", f.blockPath, 512);
+            db.AddInt64(ref cmd, "@f_lenLoc", f.lenLoc);
+            db.AddString(ref cmd, "@f_sizeLoc", f.sizeLoc, 15);
+
+            db.ExecuteNonQuery(cmd);
+        }
+
+        public string all_uncmp(int uid)
+        {
+            List<xdb_files> files = new List<xdb_files>();
+            StringBuilder sb = new StringBuilder();
+            sb.Append("select f_idSign,f_nameLoc,f_pathLoc,f_pathSvr,f_blockPath from up7_files where uid=@uid and f_complete=0 and f_delete=0;");
+            DbHelper db = new DbHelper();
+            DbCommand cmd = db.GetCommand(sb.ToString());
+
+            db.AddInInt32(cmd, "@uid", uid);
+
+            var r = db.ExecuteReader(cmd);
+            while(r.Read())
+            {
+                xdb_files f = new xdb_files();
+                f.idSign = r.GetString(0);
+                f.nameLoc = r.GetString(1);
+                f.pathLoc = r.GetString(2);
+                f.pathSvr = r.GetString(3);
+                f.blockPath = r.GetString(4);
+                files.Add(f);
+            }
+            r.Close();
+
+            if (files.Count < 1) return string.Empty;
+            return JsonConvert.SerializeObject(files);
         }
 
         public void delete(string id)
