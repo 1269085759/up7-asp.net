ecaadd0cbb4d126c756d8bd633baa58eec63f278
qwl <qwl@ncmem.com>
2017-08-05 14:25:42 +0800

    增加队列表

---
+++ demoSql2005/db/biz/database/DBFileQueue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace up7.demoSql2005.db.biz.database
+{
+    public class DBFileQueue
+    {
+    }
+}
