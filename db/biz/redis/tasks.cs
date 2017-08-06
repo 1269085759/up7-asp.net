5fa82f0a43fa9546808859ff73c00820f1ea72f9
qwl <qwl@ncmem.com>
2017-08-06 17:58:58 +0800

    精简代码

--- db/biz/redis/tasks.cs
+++ db/biz/redis/tasks.cs
@@ -1,26 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using Newtonsoft.Json;
-using up7.db.model;
 
 namespace up7.db.biz.redis
 {
     public class tasks
     {
-        public string uid = "";
-        string keyName = "tasks";
         CSRedis.RedisClient con = null;
 
         public tasks(ref CSRedis.RedisClient c) { this.con = c; }
-        string getKey() { return this.keyName + "-" + this.uid; }
-
-        public void del(string id)
-        {
-            //从队列中删除
-            this.con.SRem(this.getKey(), id);
-            //删除key
-            this.con.Del(id);
-        }
 
         /// <summary>
         /// 清除所有子文件信息
