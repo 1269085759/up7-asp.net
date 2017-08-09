0e135d6cf80b172443501f46b174e666618f0230
qwl <qwl@ncmem.com>
2017-08-09 10:49:24 +0800

    文件删除逻辑完善

--- down3/db/f_del.aspx.cs
+++ down3/db/f_del.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using up7.db.biz.redis;
+using up7.down3.biz;
 using tasks = up7.down3.biz.redis.tasks;
 
 namespace up7.down3.db
@@ -8,7 +9,7 @@ namespace up7.down3.db
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string fid = Request.QueryString["signSvr"];
+            string fid  = Request.QueryString["id"];
             string uid = Request.QueryString["uid"];
             string cbk = Request.QueryString["callback"];
 
@@ -19,9 +20,8 @@ namespace up7.down3.db
                 return;
             }
 
-            var j = RedisConfig.getCon();
-            tasks svr = new tasks(uid,j);
-            svr.del(fid);
+            DnFile db = new DnFile();
+            db.Delete(fid, int.Parse(uid));
 
             Response.Write(cbk + "({\"value\":1})");
         }
