22e63d5598dbd2dc7bd01b186304120b59c207a9
qwl <qwl@ncmem.com>
2017-08-11 09:50:17 +0800

    更新版本号

--- js/up7.js
+++ js/up7.js
@@ -65,7 +65,7 @@ function HttpUploaderMgr()
 	this.Config = {
 		  "EncodeType"		: "utf-8"
 		, "Company"			: "荆门泽优软件有限公司"
-		, "Version"			: "2,7,126,50969"
+		, "Version"			: "2,7,129,51258"
 		, "License"			: ""//
 		, "Authenticate"	: ""//域验证方式：basic,ntlm
 		, "AuthName"		: ""//域帐号
