9e0dc7f84bb0104d42d7765303701adcd7b5c7aa
qwl <qwl@ncmem.com>
2017-08-16 15:53:08 +0800

    更新下载版本号

--- down3/js/down.js
+++ down3/js/down.js
@@ -44,7 +44,7 @@ function DownloaderMgr()
 		, "Debug"		: false//调试模式
 		, "LogFile"		: "f:\\log.txt"//日志文件路径。
 		, "Company"		: "荆门泽优软件有限公司"
-		, "Version"		: "1,2,109,51262"
+		, "Version"		: "1,2,110,51262"
 		, "License"		: ""//
 		, "Cookie"		: ""//
 		, "ThreadCount"	: 1//并发数
