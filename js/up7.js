b203631dd84ca0f7a16bb0138662bb5b369b1594
xfl <xfl@ncmem.com>
2017-10-27 17:42:56 +0800

    版本号更新：2,7,141,51475

--- js/up7.js
+++ js/up7.js
@@ -65,7 +65,7 @@ function HttpUploaderMgr()
 	this.Config = {
 		  "EncodeType"		: "utf-8"
 		, "Company"			: "荆门泽优软件有限公司"
-		, "Version"			: "2,7,140,51349"
+		, "Version"			: "2,7,141,51475"
 		, "License"			: ""//
 		, "Authenticate"	: ""//域验证方式：basic,ntlm
 		, "AuthName"		: ""//域帐号
