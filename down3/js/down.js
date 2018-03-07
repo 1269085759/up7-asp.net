6651741b70a7e6308bb814a172da05ea77187dbe
qwl <qwl@ncmem.com>
2018-03-07 10:41:26 +0800

    下载页面添加文件夹路径检查逻辑

--- down3/js/down.js
+++ down3/js/down.js
@@ -311,7 +311,12 @@ function DownloaderMgr()
 	{
         var p = this.filesMap[json.id];
 	    p.down_error(json);
-	};
+    };
+    this.down_open_folder = function (json) {
+        //用户选择的路径
+        //json.path
+        this.Config["Folder"] = json.path;
+    };
 	this.down_recv_size = function (json)
 	{
         var p = this.filesMap[json.id];
@@ -361,7 +366,7 @@ function DownloaderMgr()
 	    var json = JSON.parse(str);
 	         if (json.name == "open_files") { _this.open_files(json); }
 	    else if (json.name == "init_file_cmp") { _this.init_file_cmp(json); }
-	    else if (json.name == "open_folder") { _this.open_folders(json); }
+	    else if (json.name == "open_folder") { _this.down_open_folder(json); }
 	    else if (json.name == "down_recv_size") { _this.down_recv_size(json); }
 	    else if (json.name == "down_recv_name") { _this.down_recv_name(json); }
 	    else if (json.name == "init_end") { _this.init_end(json); }
